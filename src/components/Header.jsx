69a8a3af928e829e0c6acf26a5152ffd3fa2bc80
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-01-28 11:18:35 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,11 @@ const Header = () => {
     <header className="resume-header">
       <div className="header-content">
         <h1>Mark Smith</h1>
-        <h2>Softaware Developer</h2>
+        <h2>Full Stack Developer</h2>
         <div className="social-links">
-          <a href="https://linkedin.com/in/johndoe" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="https://github.com/johndoe" target="_blank" rel="noopener noreferrer">Github</a>
-            <a 
-      href="https://github.com/johndoe" 
-      target="_blank" 
-      rel="noopener noreferrer" 
-      className="flex items-center gap-2 text-blue-500 hover:text-blue-700"
-    >
-      <FaGithub size={20} />
-      GitHub (John Doe)
-    </a>
-
-          <p><b>📞+123 456 7890</b> | <b>✉️ marksmith@example.com</b> </p>
+          <a href="https://linkedin.com/in/makrsmith" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://github.com/makrsmith" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <p><b>📞 +123 456 7890</b> | <b>✉️ makrsmith@example.com</b> </p>
         </div>
         
       </div>
